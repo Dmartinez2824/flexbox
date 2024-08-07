ca8dcb7b5cfaf9f1ffe081c74e1c1d93787c2b7d
Daniel Martinez <dafer4563@gmail.com>
2024-08-06 20:54:00 -0500

    agg js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+alert("bienvenidos aa mi pag")
+const link = document.querySelectorAll(".links");
+const input = document.querySelector(".nav__input");
+
+
+link.forEach(element => {
+    element.addEventListener("click",function(event){
+        if(input.checked){
+            input.checked = false;
+        }
+    
+    })
+});
